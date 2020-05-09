b2a3f33d8f821ea69eb16b7d5c9028ffea9e45b3
Yura <wa7er14@gmail.com>
2020-05-09 14:33:33 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
-
-function burger() {
+/*eslint-disable */ //error *'burger' is defined but never used  no-unused-vars*, but it's used in button's onClick
+function burger() { 
   const elem = document.getElementById('header');
 
   elem.classList.toggle('header--active');
